c53f0d1098ce9e182da88015df4c137a82242395
Bhargavi Alimilli <bhargavi.alimilli@gmail.com>
2022-04-18 16:52:51 +0530

    update for edit notes

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -164,7 +164,7 @@ export const updateNoteAction = (id, title, content, category) => async (
     };
 
     const { data } = await axios.patch(
-      `${process.env.REACT_APP_API}/notes/update/${id}`,
+      `https://simplify-notes-server.herokuapp.com/api/notes/update/${id}`,
       { title, content, category },
       config
     );
